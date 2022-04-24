737b74f5001b49f42ce88a811d37f22e1d2e378a
Lucas <lucasazzollinivieira@gmail.com>
2022-04-23 23:21:08 -0300

    feat: signIn done

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { UserData } from "../interfaces/index.js";
 import * as authService from "../services/authService.js";
 
 export async function signUp(req: Request, res: Response) {
@@ -14,4 +15,12 @@ export async function signUp(req: Request, res: Response) {
     await authService.signUp({ password, email });
 
     res.sendStatus(201);
+}
+
+export async function signIn(req: Request, res: Response) {
+    const { password, email }: UserData = req.body;
+
+    const userData = await authService.signIn({ password, email });
+
+    res.send(userData).status(200);
 }
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,8 @@
 import * as authRepository from "../repositories/authRepository.js";
 import { UserData } from "../interfaces/index.js";
 import bcrypt from "bcrypt";
-//import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import "../setup.js";
 
 export async function signUp({ password, email }: UserData) {
     const user = await authRepository.findUserByEmail(email);
@@ -15,5 +16,34 @@ export async function signUp({ password, email }: UserData) {
 
     const hashPassword = bcrypt.hashSync(password, 10);
 
-    await authRepository.insertUser({ password: hashPassword, email })
+    await authRepository.insertUser({ password: hashPassword, email });
+}
+
+export async function signIn({ password, email }: UserData) {
+    const user = await authRepository.findUserByEmail(email);
+
+    if (!user) {
+        throw {
+            type: "not_found",
+            message: "credentials invalid"
+        }
+    }
+
+    const doesPasswordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!doesPasswordMatch) {
+        throw {
+            type: "unauthorized",
+            message: "credentials invalid"
+        }
+    }
+
+    const secretKey = process.env.JWT_SECRET;
+    const token = jwt.sign(user.id.toString(), secretKey);
+
+    await authRepository.insertSession({ token, userId: user.id });
+
+    delete user.password;
+
+    return { ...user, token }
 }
